17af20e9c16b19b459ab3b11ad38b4b47bf776cb
Gustaf Mossakowski <gustaf@koenige.org>
2023-01-10 14:04:24 +0100

    download: code style, remove unused global declaration

--- zzbrick_request/download.inc.php
+++ zzbrick_request/download.inc.php
@@ -8,7 +8,7 @@
  * https://www.zugzwang.org/modules/downloads
  *
  * @author Gustaf Mossakowski <gustaf@koenige.org>
- * @copyright Copyright © 2017-2022 Gustaf Mossakowski
+ * @copyright Copyright © 2017-2023 Gustaf Mossakowski
  * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
  */
 
@@ -103,7 +103,6 @@ function mod_downloads_download($vars, $settings, $event) {
 }
 
 function mod_downloads_download_files($vars, $settings, $event) {
-	global $zz_setting;
 	wrap_https_redirect();
 
 	if (count($vars) !== 3) return false;
